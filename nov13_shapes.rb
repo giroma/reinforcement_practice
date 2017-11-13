ae2937a77288998cfb8aec8e62e58bd9c5dcac92
Argir Argirov <dj.giro@gmail.com>
2017-11-12 22:20:39 -0500

    make an options hash with coresponding keys and values

---
+++ nov13_shapes.rb
@@ -0,0 +1,20 @@
+options = {
+  rows: 3,
+  cols: 9,
+  char: '0'
+}
+
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+puts draw_shape(options)
